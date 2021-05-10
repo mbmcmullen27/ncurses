d80c2e0c1349eb1d383aadc7044f027bb251e7d5
mbmcmullen27 <mbmcmullen27@gmail.com>
2021-05-09 19:04:56 -0500

    chtype

---
+++ chapter4/addchstr.c
@@ -0,0 +1,21 @@
+#include <ncurses.h>
+
+int main(){
+
+    chtype string[] = {
+        'H' | A_BOLD, 'e', 'l' | A_REVERSE,
+        'l' | A_REVERSE, 'o', '!' | A_UNDERLINE,
+        0
+    };
+    int x = 0;
+
+    initscr();
+
+    addchstr(string);
+
+    refresh();
+    getch();
+
+    endwin();
+    return(0);
+}
