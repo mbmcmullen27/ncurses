f301808b1e23279ee3e91b23fb91d26236d1575e
mbmcmullen27 <mbmcmullen27@gmail.com>
2021-05-16 10:22:15 -0500

    finish 8

---
+++ chapter8/flush.c
@@ -0,0 +1,20 @@
+#include <ncurses.h>
+
+int main(){
+
+    char buffer[81];
+
+    initscr();
+
+    addstr("Type. I'll wait...\n");
+    refresh();
+    napms(5000);
+
+    addstr("Flushing buffer.\n");
+    flushinp();
+    addstr("Here is what you typed:\n");
+    getnstr(buffer,80);
+
+    endwin();
+    return(0);
+}
